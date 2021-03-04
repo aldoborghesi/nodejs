72baac41f85152781fd7399805bf0d0c20ef38e9
aldoborghesi <aldoborghesigmail.com>
2021-03-03 23:28:49 -0300

    Ajustando MSG Globais

--- config/database.js
+++ config/database.js
@@ -1,2 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/db_finance', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Error.messages.Number.min = "O valor minimo aceito é de '{MIN}'"
+mongoose.Error.messages.Number.max = "O valor máximo aceito é de '{MAX}'"
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
