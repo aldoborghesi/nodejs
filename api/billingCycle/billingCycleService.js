e5674decefe9e86cc019ca9c5166329f67edb8b4
aldoborghesi <aldoborghesigmail.com>
2021-03-06 10:57:44 -0300

    Summary Cycle, Project mongoose, complicado

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,4 +2,17 @@ const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+// Rotas restful express
+BillingCycle.route('count', function(req, res, next) {
+  //api mongoose
+  BillingCycle.count(function(error, value){
+    if(error) {
+      // retorna um objeto coo resposta, atributo errors e um array 'error'
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json({value})
+    }
+  })
+})
 module.exports = BillingCycle
